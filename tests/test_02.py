4da666c05ddc09705c7c0402dcbda10fca4b5d07
lmpizarro <lmpizarro@gmail.com>
2019-10-01 23:57:04 -0300

    test relation

--- tests/test_02.py
+++ tests/test_02.py
@@ -3,7 +3,9 @@ import os
 import datetime
 from datetime import timedelta
 
-t2 = timedelta(weeks=0, days=365, hours=0, seconds=0)
+import numpy as np
+
+t2 = timedelta(weeks=0, days=360, hours=0, seconds=0)
 
 begin = str(datetime.date.today() - t2)
 
@@ -15,7 +17,8 @@ print(sys.path)
 from ticker import Ticker
 from matplotlib import pyplot as plt
 
-if __name__ == '__main__':
+
+def test_01():
     tickers = ['^MERV', 'ALUA', 'BMA', 'BBAR', 'BYMA', 'CVH', 'CEPU', 'CRES', 'EDN', 'GGAL', 'VALO', 'SUPV',
                'MIRG', 'PAMP', 'COME', 'TECO2', 'TXAR', 'TRAN', 'TGNO4', 'TGSUD2', 'YPFD', 'TS', 'APBR']
 
@@ -42,3 +45,45 @@ if __name__ == '__main__':
 
     plt.plot(vol)
     plt.show()
+
+def relation_US_BA(ticker, ticker_ba):
+    tck_ba = Ticker(ticker_ba, begin=begin)
+    tck = Ticker(ticker, begin=begin)
+
+    tck.get_historical_price_data()
+    tck_ba.get_historical_price_data()
+
+    prices = {}
+    for price in tck.historical_data['prices']:
+        fd = price['formatted_date']
+        prices[fd] = price
+
+    prices_ba = {}
+    for price in tck_ba.historical_data['prices']:
+        fd = price['formatted_date']
+        prices_ba[fd] = price
+
+    rel = []
+    for pk in prices_ba:
+        if pk in prices:
+            print({'date': pk, 'price_ba': prices_ba[pk]['adjclose'], 'price': prices[pk]['adjclose']})
+            try:
+                rel.append(prices[pk]['adjclose']/prices_ba[pk]['adjclose'])
+            except Exception as e:
+                print(e)
+
+
+    rel = np.asarray(rel)
+
+    return rel
+
+if __name__ == '__main__':
+    tickers = [('BMA','BMA.BA'), ('TS', 'TS.BA'), ('YPF', 'YPFD.BA'), ('GGAL', 'GGAL.BA'), ('BBAR', 'BBAR.BA'), ('PBR', 'APBR.BA')]
+
+    tickers_ = []    # ('BBAR', 'BBAR.BA')]
+    tickers_ = [('TS','TEN.MI')]    # ('BBAR', 'BBAR.BA')]
+    for tick in tickers:
+        rel = relation_US_BA(tick[0], tick[1])
+        plt.plot(rel)
+        plt.show()
+
